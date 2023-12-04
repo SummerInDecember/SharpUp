0a95c83fa493be8e26063972a7f89ce2f677e997
Summer-In-December <gabriel@IDONTEXIST.home>
2023-12-04 22:01:12 +0000

    first commit

---
+++ console/SharpUp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SharpUp
+{
+    class MainConsoleClass
+    {
+        public static void Options()
+        {
+            Console.WriteLine("-h        Show this menu");
+
+        }
+        public static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
